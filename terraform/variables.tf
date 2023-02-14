95e775f0eb4557b1d5154b37ddff6118e4ea4e64
Aslam Ahemad <aslamahemad@gmail.com>
2023-02-14 13:16:43 +0530

    initial commit

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable vpc_cidr_block {
+    default = "10.0.0.0/16"
+}
+variable subnet_cidr_block {
+    default = "10.0.10.0/24"
+}
+variable avail_zone {
+    default = "us-east-1a"
+}
+variable env_prefix {
+    default = "dev"
+}
+variable my_ip {
+    default = "192.168.213.1/32"
+}
+variable jenkins_ip {
+    default = "44.193.3.8/32"
+}
+variable instance_type {
+    default = "t2.micro"
+}
+variable region {
+    default = "us-east-1"
+}
